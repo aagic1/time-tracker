a4f9243e8981ad45d689b3b6b13156f908dfb732
Ahmed Agić <agic.ahmed3@gmail.com>
2023-09-11 12:59:17 +0200

    Rearange code and fix template string

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
-import express, { Express, Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
+
+import express, { Express, Request, Response, Application } from 'express';
 
 import activitiesRouter from './src/routes/activities';
-
-dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
@@ -15,5 +15,5 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/v1/activities', activitiesRouter);
 
 app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:{port}`);
+  console.log(`Server is listening on http://localhost:${port}`);
 });
