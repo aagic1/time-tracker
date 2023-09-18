dfcd5f58eeda455eb8e004e29dff9207e7ee0628
Ahmed Agić <agic.ahmed3@gmail.com>
2023-09-18 21:47:02 +0200

    Handle and format database errors

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -2,6 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomApiError } from '../errors/custom-api-error';
 import { DatabaseError } from 'pg';
 
+const dbErrorCodes = {
+  unique: '23505',
+};
+
 export function errorHandler(
   error: CustomApiError | DatabaseError | Error | string,
   req: Request,
@@ -19,7 +23,21 @@ export function errorHandler(
     });
   } else if (error instanceof DatabaseError) {
     console.log('DB error');
-    res.json(error);
+
+    if (error.code === dbErrorCodes.unique) {
+      let [source, value] = error.detail!.split('=').map((s) => {
+        return s.slice(s.indexOf('(') + 1, s.indexOf(')'));
+      });
+      source = source.charAt(0).toUpperCase() + source.slice(1);
+      res.status(409).json({
+        error: {
+          type: 'ConflictError',
+          message: `${source} with value ${value} already exists`,
+        },
+      });
+    } else {
+      res.status(400).json(error);
+    }
   } else if (error instanceof Error) {
     console.log('error');
     res.json({ error });
