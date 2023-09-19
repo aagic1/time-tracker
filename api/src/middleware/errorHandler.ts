2fe3eadf859e08c1bc14cf3ae86ebbc4015186c6
Ahmed Agić <agic.ahmed3@gmail.com>
2023-09-19 17:48:55 +0200

    Clean up error handling middleware code

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -2,10 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomApiError } from '../errors/custom-api-error';
 import { DatabaseError } from 'pg';
 
-const dbErrorCodes = {
-  unique: '23505',
-};
-
 export function errorHandler(
   error: CustomApiError | DatabaseError | Error | string,
   req: Request,
@@ -14,35 +10,42 @@ export function errorHandler(
 ) {
   if (error instanceof CustomApiError) {
     console.log('custom error');
-    res.status(error.statusCode).json({
-      error: {
-        message: error.message,
-        type: error.name,
-        issues: error.issues,
-      },
-    });
+    res
+      .status(error.statusCode)
+      .json({ error: { ...error, message: error.message } });
   } else if (error instanceof DatabaseError) {
     console.log('DB error');
-
-    if (error.code === dbErrorCodes.unique) {
-      let [source, value] = error.detail!.split('=').map((s) => {
-        return s.slice(s.indexOf('(') + 1, s.indexOf(')'));
-      });
-      source = source.charAt(0).toUpperCase() + source.slice(1);
-      res.status(409).json({
-        error: {
-          type: 'ConflictError',
-          message: `${source} with value ${value} already exists`,
-        },
-      });
-    } else {
-      res.status(400).json(error);
-    }
+    const dbError = parseDatabaseError(error);
+    res.status(409).json({ error: dbError });
   } else if (error instanceof Error) {
     console.log('error');
     res.json({ error });
   } else if (typeof error === 'string') {
     console.log('string');
     res.json(error);
+  }
+}
+
+const dbErrorCodes = {
+  unique: '23505',
+};
+
+function parseDatabaseError(error: DatabaseError) {
+  if (error.code === dbErrorCodes.unique) {
+    console.log(error);
+    let domainName =
+      error.table!.charAt(0).toUpperCase() + error.table!.slice(1);
+    let [sources, values] = error
+      .detail!.split('=')
+      .map((s) => {
+        return s.slice(s.indexOf('(') + 1, s.indexOf(')'));
+      })
+      .map((s) => s.split(', '));
+    return {
+      type: 'ConflictError',
+      message: `${domainName} with value(s) (${sources})=(${values}) already exists`,
+    };
+  } else {
+    return 'Unhandled db error';
   }
 }
