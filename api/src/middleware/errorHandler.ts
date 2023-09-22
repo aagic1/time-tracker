4cf0aa24dffd1e2ab64397afeb37311056238b8b
Ahmed Agić <agic.ahmed3@gmail.com>
2023-09-22 20:24:10 +0200

    Handle database foreign key and range errors

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -8,6 +8,7 @@ export function errorHandler(
   res: Response,
   next: NextFunction
 ) {
+  console.log(error);
   if (error instanceof CustomApiError) {
     console.log('custom error');
     res
@@ -28,22 +29,38 @@ export function errorHandler(
 
 const dbErrorCodes = {
   unique: '23505',
+  foreignKey: '23503',
+  outOfRangeNumeric: '22003',
 };
+
+function parseDatabaseErrorContents(error: DatabaseError) {
+  let domainName = error.table!.charAt(0).toUpperCase() + error.table!.slice(1);
+  let [sources, values] = error
+    .detail!.split('=')
+    .map((s) => {
+      return s.slice(s.indexOf('(') + 1, s.indexOf(')'));
+    })
+    .map((s) => s.split(', '));
+  return { domainName, sources, values };
+}
 
 function parseDatabaseError(error: DatabaseError) {
   if (error.code === dbErrorCodes.unique) {
-    console.log(error);
-    let domainName =
-      error.table!.charAt(0).toUpperCase() + error.table!.slice(1);
-    let [sources, values] = error
-      .detail!.split('=')
-      .map((s) => {
-        return s.slice(s.indexOf('(') + 1, s.indexOf(')'));
-      })
-      .map((s) => s.split(', '));
+    const { domainName, sources, values } = parseDatabaseErrorContents(error);
     return {
       type: 'ConflictError',
       message: `${domainName} with value(s) (${sources})=(${values}) already exists`,
+    };
+  } else if (error.code === dbErrorCodes.foreignKey) {
+    const { domainName, sources, values } = parseDatabaseErrorContents(error);
+    return {
+      type: 'NotFoundError',
+      message: `Key (${sources}): (${values}) doesn't exist`,
+    };
+  } else if (error.code === dbErrorCodes.outOfRangeNumeric) {
+    return {
+      type: 'NumericOutOfRangeError',
+      message: `Some numeric value is out of range. This message is bad, should be more specific. But error doesn't give any info`,
     };
   } else {
     return 'Unhandled db error';
