5d8a887216cac94faf688699225d8760ed32e5c4
Ahmed Agić <agic.ahmed3@gmail.com>
2023-09-28 11:12:45 +0200

    Parse timestamps from database as ISO dates

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,6 +1,9 @@
 import { Kysely, PostgresDialect } from 'kysely';
 import { DB } from './types';
-import { Pool } from 'pg';
+import { Pool, types } from 'pg';
+
+const timestampOID = 1114;
+types.setTypeParser(timestampOID, (stringValue) => new Date(stringValue + 'Z'));
 
 export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
