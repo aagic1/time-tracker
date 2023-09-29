54f6825ac656ce5156562e72e955c804f089bd51
Ahmed Agić <agic.ahmed3@gmail.com>
2023-09-29 20:33:11 +0200

    Use PATCH instead of POST method to reset password

--- api/src/auth/auth.router.ts
+++ api/src/auth/auth.router.ts
@@ -16,7 +16,7 @@ authRouter.route('/logout').post(logout);
 authRouter.route('/login').post(checkNotAuthenticated, login);
 authRouter.route('/register').post(register);
 authRouter.route('/forgot-password').post(forgotPassword);
-authRouter.route('/reset-password').post(resetPassword);
+authRouter.route('/reset-password').patch(resetPassword);
 authRouter.route('/verify-email/resend').post(resendVerificationCode);
 authRouter.route('/verify-email').patch(verifyEmail);
 
