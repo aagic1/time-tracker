3edc7cabe1afedb09b8382b5f8b0857796967e52
Ahmed Agić <agic.ahmed3@gmail.com>
2023-12-14 00:17:01 +0100

    Implement forgot password functionality

--- frontend/src/pages/Auth/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/pages/Auth/ForgotPassword/ForgotPassword.jsx
@@ -1,23 +1,29 @@
-import { Form } from 'react-router-dom';
+import { Form, redirect } from 'react-router-dom';
 // import styles from './forgot-password.module.css';
 import styles from '../auth-form.module.css';
 
 export async function action({ request }) {
   const formData = await request.formData();
-  const email = formData.email;
-  const res = await fetch('http://localhost:8000/api/v1/auth/forgot-password', {
-    method: 'POST',
-    body: { email },
-  });
-  if (res.status !== 200) {
-    return null;
+  const email = formData.get('email');
+  const response = await fetch(
+    'http://localhost:8000/api/v1/auth/forgot-password',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: email }),
+    }
+  );
+  if (response.status !== 200) {
+    return await response.json();
   }
-  return await res.json();
+  return redirect(`../forgot-password-confirmation?email=${email}`);
 }
 
 export default function ForgotPassword() {
   return (
-    <Form method="POST" className={styles.authForm}>
+    <Form method="post" className={styles.authForm}>
       <p className={styles.message}>
         Please provide the email adress that you used when you registered up for
         your account.
@@ -27,7 +33,9 @@ export default function ForgotPassword() {
         <input type="text" name="email" id="email" />
       </div>
       <div className={styles.buttonContainer}>
-        <button className={styles.confirmButton}>Confirm</button>
+        <button type="submit" className={styles.confirmButton}>
+          Confirm
+        </button>
       </div>
     </Form>
   );
