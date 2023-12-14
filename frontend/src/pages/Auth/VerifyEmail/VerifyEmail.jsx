33ab5c34d3a2b238ac06eee73fb3d4f95ddd8e34
Ahmed Agić <agic.ahmed3@gmail.com>
2023-12-14 14:11:09 +0100

    Implement email verification "happy path"

--- frontend/src/pages/Auth/VerifyEmail/VerifyEmail.jsx
+++ frontend/src/pages/Auth/VerifyEmail/VerifyEmail.jsx
@@ -1,44 +1,70 @@
-import { Form, redirect } from 'react-router-dom';
+import { Form, redirect, useSearchParams } from 'react-router-dom';
 // import styles from './verify-email.module.css';
 import styles from '../auth-form.module.css';
 
 export async function action({ request }) {
-  const formData = await request.formData();
-  const code = formData.get('code');
+  if (request.method === 'PATCH') {
+    const formData = await request.formData();
+    const code = formData.get('code');
 
-  const res = await fetch('http://localhost:8000/api/v1/auth/verify-email', {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ token: code }),
-  });
+    const res = await fetch('http://localhost:8000/api/v1/auth/verify-email', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ token: code }),
+    });
 
-  if (!res.ok) {
-    return await res.json();
+    if (!res.ok) {
+      return await res.json();
+    }
+    return redirect('/login');
+  } else {
+    const url = new URL(request.url);
+    const email = url.searchParams.get('email');
+    const res = await fetch(
+      'http://localhost:8000/api/v1/auth/verify-email/resend',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      }
+    );
+
+    if (!res.ok) {
+      return await res.json();
+    }
+    return true;
   }
-  return redirect('/login');
 }
 
 export default function VerifyEmail() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   return (
-    <Form method="patch" className={styles.authForm}>
-      <p className={styles.message}>
-        We sent an email verification code to "email".
-      </p>
-      <p className={styles.message}>Please enter the code below.</p>
-      <div className={styles.inputContainer}>
-        <label htmlFor="code">Verification code:</label>
-        <input type="text" name="code" id="code" />
-      </div>
-      <div className={styles.buttonContainer}>
-        <button className={styles.confirmButton}>Verify</button>
-      </div>
-      <div className={styles.resendButtonContainer}>
-        <button className={styles.confirmButton}>
-          Resend verification code
-        </button>
-      </div>
-    </Form>
+    <>
+      <Form method="patch" className={styles.authForm}>
+        <p className={styles.message}>
+          We sent an email verification code to {searchParams.get('email')}.
+        </p>
+        <p className={styles.message}>Please enter the code below.</p>
+        <div className={styles.inputContainer}>
+          <label htmlFor="code">Verification code:</label>
+          <input type="text" name="code" id="code" />
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.confirmButton}>Verify</button>
+        </div>
+      </Form>
+      <Form method="post">
+        <div className={styles.resendButtonContainer}>
+          <button className={styles.confirmButton}>
+            Resend verification code
+          </button>
+        </div>
+      </Form>
+    </>
   );
 }
