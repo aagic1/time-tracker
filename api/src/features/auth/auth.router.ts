73361bbb15d306a648b0b434535db2193cbd45fc
Ahmed Agić <agic.ahmed3@gmail.com>
2024-01-10 13:19:10 +0100

    Add whoami route for checking if user is logged in

--- api/src/features/auth/auth.router.ts
+++ api/src/features/auth/auth.router.ts
@@ -20,4 +20,12 @@ authRouter.route('/reset-password').patch(resetPassword);
 authRouter.route('/verify-email/resend').post(resendVerificationCode);
 authRouter.route('/verify-email').patch(verifyEmail);
 
+authRouter.route('/whoami').get((req, res) => {
+  console.log(req.session);
+  if (!req.session || !req.session.user) {
+    return res.json(null);
+  }
+  return res.json(req.session.user?.email);
+});
+
 export default authRouter;
