e513710ce20437b298a2ef86ab802dc2b02a9a76
Ahmed Agić <agic.ahmed3@gmail.com>
2024-01-14 17:45:49 +0100

    Remove custom parsing of postgres timestamps

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -2,9 +2,6 @@ import { Kysely, PostgresDialect } from 'kysely';
 import { DB } from './types';
 import { Pool, types } from 'pg';
 
-const timestampOID = 1114;
-types.setTypeParser(timestampOID, (stringValue) => new Date(stringValue + 'Z'));
-
 export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
     pool: new Pool({
