97a861c0ff2f6c6f2bcbfe86a4fb9def7ab94930
Ahmed Agić <agic.ahmed3@gmail.com>
2024-01-14 17:47:05 +0100

    Create custom parser for postgres interval

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,6 +1,19 @@
 import { Kysely, PostgresDialect } from 'kysely';
 import { DB } from './types';
 import { Pool, types } from 'pg';
+import parse from 'postgres-interval';
+
+function parseInterval(interval: string) {
+  let parsed = parse(interval);
+  return {
+    hours: parsed.hours + parsed.days * 24,
+    minutes: parsed.minutes,
+    seconds: parsed.seconds,
+    miliseconds: parsed.milliseconds,
+  };
+}
+
+types.setTypeParser(types.builtins.INTERVAL, parseInterval);
 
 export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
