5f817a7d634eb903aea2223cdb78377b2bcef916
Ahmed Agić <agic.ahmed3@gmail.com>
2024-02-20 17:43:16 +0100

    Throw error when accessing forbidden resource

--- api/src/middleware/authenticate.ts
+++ api/src/middleware/authenticate.ts
@@ -1,24 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
-import { UnauthenticatedError } from '../errors/not-authenticated.error';
+import { UnauthenticatedError } from '../errors/not-authenticated-error';
+import { ForbiddenError } from '../errors/forbidden-error';
 
-export function checkAuthenticated(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function checkAuthenticated(req: Request, res: Response, next: NextFunction) {
   if (!req.session || !req.session.user) {
     throw new UnauthenticatedError('Not authenticated. Please log in first');
   }
   next();
 }
 
-export function checkNotAuthenticated(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function checkNotAuthenticated(req: Request, res: Response, next: NextFunction) {
   if (req.session && req.session.user) {
-    return res.json({ msg: 'Can not log in or register if already logged in' });
+    throw new ForbiddenError('Can not log in or register if already logged in');
   }
   next();
 }
