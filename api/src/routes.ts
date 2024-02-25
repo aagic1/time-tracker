5c6cc9487c9de07a3594697ea376b9bcaec40b6f
Ahmed Agić <agic.ahmed3@gmail.com>
2024-02-26 00:29:40 +0100

    Clean up code
    - Remove commented unused code
    - Add comments

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -14,12 +14,12 @@ apiRouter.get('/', (req: Request, res: Response) => {
 
 apiRouter.use('/auth', authRouter);
 
-// apiRouter.use(checkAuthenticated);
-
 // protected routes - only logged in users with valid session
 apiRouter.use('/users', checkAuthenticated, userRouter);
 apiRouter.use('/activities', checkAuthenticated, activitiesRouter);
 apiRouter.use('/records', checkAuthenticated, recordsRouter);
+
+// NOT FOUND route
 apiRouter.use('*', (req: Request, res: Response) => {
   res.status(404).send('Not Found');
 });
