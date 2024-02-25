6bb8766b22e098f0e658a9bf6887c634b2040a8f
Ahmed Agić <agic.ahmed3@gmail.com>
2024-02-26 00:27:33 +0100

    Add redis session store
    Use redis for storing session data instead of the default session store.

--- api/src/middleware/session.ts
+++ api/src/middleware/session.ts
@@ -1,12 +1,26 @@
+import RedisStore from 'connect-redis';
 import session from 'express-session';
+import { createClient } from 'redis';
 
+// ession object
 declare module 'express-session' {
   interface SessionData {
     user: { id: bigint; email: string };
   }
 }
 
+// Initialize client
+let redisClient = createClient({ url: 'redis://127.0.0.1:6379' });
+redisClient.connect().catch(console.error);
+
+// Initialize store
+let redisStore = new RedisStore({
+  client: redisClient,
+});
+
+// Define session
 export default session({
+  store: redisStore,
   secret: process.env.EXPRESS_SESSION_SECRET as string,
   saveUninitialized: false,
   resave: false,
