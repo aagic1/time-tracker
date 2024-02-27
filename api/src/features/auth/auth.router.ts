3bee90bc90e529f9ea8e5747002ade32006d84bc
Ahmed Agić <agic.ahmed3@gmail.com>
2024-02-27 18:25:12 +0100

    Fix whoami bug
    Move whoami endpoint before using middleware that limits route access
    only to unauthenticated users. whoami route should be accessible by all
    users (logged in or not).

--- api/src/features/auth/auth.router.ts
+++ api/src/features/auth/auth.router.ts
@@ -13,23 +13,6 @@ import { checkNotAuthenticated, checkAuthenticated } from '../../middleware/auth
 
 const authRouter = express.Router();
 
-// routes accessible only to users that ARE logged in
-authRouter.route('/logout').post(checkAuthenticated, logout);
-
-// routes accessible only to users that ARE NOT logged in
-const notAuthenticatedRouter = express.Router();
-notAuthenticatedRouter.use(checkNotAuthenticated);
-
-notAuthenticatedRouter.route('/login').post(login);
-notAuthenticatedRouter.route('/register').post(register);
-notAuthenticatedRouter.route('/verify-email').patch(verifyEmail);
-notAuthenticatedRouter.route('/verify-email/resend').post(resendVerificationCode);
-notAuthenticatedRouter.route('/forgot-password/initiate').post(sendPasswordRecoveryCode);
-notAuthenticatedRouter.route('/forgot-password/code').post(verifyPasswordRecoveryCode);
-notAuthenticatedRouter.route('/forgot-password/password').patch(resetPassword);
-
-authRouter.use(notAuthenticatedRouter);
-
 // routes accessible to ALL users (whether logged in or not)
 authRouter.route('/whoami').get((req, res) => {
   if (!req.session || !req.session.user) {
@@ -37,6 +20,19 @@ authRouter.route('/whoami').get((req, res) => {
   }
   return res.json(req.session.user?.email);
 });
+
+// routes accessible only to users that ARE logged in
+authRouter.route('/logout').post(checkAuthenticated, logout);
+
+// routes accessible only to users that ARE NOT logged in
+authRouter.use(checkNotAuthenticated);
+authRouter.route('/login').post(login);
+authRouter.route('/register').post(register);
+authRouter.route('/verify-email').patch(verifyEmail);
+authRouter.route('/verify-email/resend').post(resendVerificationCode);
+authRouter.route('/forgot-password/initiate').post(sendPasswordRecoveryCode);
+authRouter.route('/forgot-password/code').post(verifyPasswordRecoveryCode);
+authRouter.route('/forgot-password/password').patch(resetPassword);
 
 // ________
 // public API
