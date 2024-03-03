4837a1763f78c3ab4efd0fa9efc95838515289e0
Ahmed Agić <agic.ahmed3@gmail.com>
2024-03-03 18:43:22 +0100

    Feat: create email message generator function

--- api/src/features/auth/auth.utils.ts
+++ api/src/features/auth/auth.utils.ts
@@ -4,12 +4,28 @@ import jwt from 'jsonwebtoken';
 
 import { Message } from './auth.types';
 
-export function generateJWT(email: string, type: Message, expiresIn = '30m') {
+function generateJWT(email: string, type: Message, expiresIn = '30m') {
   return jwt.sign({ email, type }, process.env.JWT_SECRET!, {
     expiresIn,
   });
 }
 
-export function generateUUID() {
+function generateUUID() {
   return crypto.randomUUID();
 }
+
+function generateEmailMessage(type: Message, redirectURL: string) {
+  let message;
+  if (type === 'Email verification') {
+    message = 'Click on the following link to reset your password:';
+  } else {
+    message = 'Click on the following link to verify your account:';
+  }
+  return `
+    <h2>${type}</h2>
+    <p>${message}</p>
+    <a href=${redirectURL}>${type}</a>
+  `;
+}
+
+export { generateJWT, generateUUID, generateEmailMessage };
