d9a718fc102e439ef54d83242c9d4a88606fe77a
ahmedagi <agic.ahmed3@gmail.com>
2024-03-21 00:33:58 +0100

    Rearange middleware

--- api/index.ts
+++ api/index.ts
@@ -12,21 +12,23 @@ import { errorHandler } from './src/middleware/errorHandler';
 
 const app: Application = express();
 
-// middleware
-app.use(express.json());
-app.use(session);
-app.use(helmet());
-
+app.set('trust proxy', 1);
 let origin = process.env.CORS_ORIGIN!;
 if (origin.slice(-1) === '/') {
   origin = origin.slice(0, -1);
 }
+// middleware
 app.use(
   cors({
     credentials: true,
     origin: origin,
+    optionsSuccessStatus: 200,
   })
 );
+app.use(express.json());
+app.use(helmet());
+
+app.use(session);
 
 // routes
 app.use('/api/v1', apiRouter);
