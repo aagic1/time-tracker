337aba97ec76b9998e9c0fed0c93708bb7f94b1e
ahmedagi <agic.ahmed3@gmail.com>
2024-03-21 23:30:40 +0100

    Change createActivityCard appearance

--- frontend/src/features/activities/components/CreateActivityCard/CreateActivityCard.jsx
+++ frontend/src/features/activities/components/CreateActivityCard/CreateActivityCard.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
+import { FiPlusCircle } from 'react-icons/fi';
 
 import styles from './create-activity-card.module.css';
 
 export function CreateActivityCard() {
   return (
     <Link className={styles.cardContainer} to={`create`}>
-      <div>+</div>
-      <div>New activity</div>
+      <div>
+        <FiPlusCircle className={styles.plus} />
+      </div>
+      <div>Add</div>
     </Link>
   );
 }
