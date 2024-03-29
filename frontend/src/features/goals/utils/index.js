01633363ac2af68af0d6655fc369e18ec2fce030
ahmedagi <agic.ahmed3@gmail.com>
2024-03-29 18:15:02 +0100

    Refactor: remove duplicate code (DRY)

--- frontend/src/features/goals/utils/index.js
+++ frontend/src/features/goals/utils/index.js
@@ -1,45 +1,7 @@
-function intervalToString(interval) {
-  const hours = interval.hours;
-  const minutes = interval.minutes;
-  const seconds = interval.seconds;
-
-  let timeString = '';
-  if (hours) {
-    timeString += hours + 'h ';
-  }
-  if (minutes || hours) {
-    timeString += minutes + 'm ';
-  }
-  if (!minutes && !hours) {
-    timeString += seconds + 's';
-  }
-
-  return timeString;
-}
-
-function intervalToSeconds(interval) {
-  return interval.hours * 60 * 60 + interval.minutes * 60 + interval.seconds;
-}
-
-function intervalToMiliseconds(interval) {
-  return interval.miliseconds + 1000 * intervalToSeconds(interval);
-}
-
-function secondsToInterval(totalSeconds) {
-  const hours = Math.trunc(totalSeconds / (60 * 60));
-  const minutes = Math.trunc((totalSeconds % (60 * 60)) / 60);
-  const seconds = totalSeconds % 60;
-  return { hours, minutes, seconds };
-}
+import { intervalToMiliseconds } from '../../../utils/interval';
 
 function getRemainingGoalTime(goal, elapsedTime) {
   return intervalToMiliseconds(goal) - elapsedTime;
 }
 
-export {
-  intervalToString,
-  intervalToSeconds,
-  intervalToMiliseconds,
-  secondsToInterval,
-  getRemainingGoalTime,
-};
+export { getRemainingGoalTime };
