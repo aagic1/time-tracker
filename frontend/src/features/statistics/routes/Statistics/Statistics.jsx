1f4a9fd1287ddf070cc6af743a1e4462ba77f567
ahmedagi <agic.ahmed3@gmail.com>
2024-04-09 00:10:46 +0200

    Make stats filter component sticky

--- frontend/src/features/statistics/routes/Statistics/Statistics.jsx
+++ frontend/src/features/statistics/routes/Statistics/Statistics.jsx
@@ -101,6 +101,11 @@ export function Statistics() {
         {stats.toReversed().map((entry) => (
           <StatisticsCard key={entry.activityId} data={entry} percentage={entry.percent} />
         ))}
+        <div className={styles.horizontalLine}></div>
+        <StatisticsCard
+          data={{ totalTime: sumOfTotalTimes, color: '#fff', activityName: 'Total time' }}
+          showPercentage={false}
+        />
       </div>
 
       <div className={styles.filterContainer}>
